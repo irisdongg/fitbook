d4ac0d5a3dc019a81e4d8392777fd83f834901f7
Emma Herrera <emadilla@Emmas-MacBook-Air.local>
2020-09-26 14:08:02 -0700

    login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-import Login from "./Login.js";
 
-function App() {
-  return (
-    <main className="App">
-        <Login/>;
-    </main>
-  );
+import {Button, Form, FormGroup, Label, Input}
+  from 'reactstrap';
+
+import {FacebookLoginButton} from 'react-social-login-buttons';
+import {GoogleLoginButton} from 'react-social-login-buttons';
+
+class App extends Component {
+  render() {
+    return (
+      <Form className="login-form">
+        <h1>
+          <span className="font-weight-bold">fitbook</span>.com
+        </h1>
+        <h2 className="text-center">Welcome</h2>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input type="email" placeholder="Email"/>
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <Input type="password" placeholder="Password"/>
+        </FormGroup>
+        <Button className="btn-lg btn-dark btn-block">
+          Log in
+        </Button>
+        <div className="text-center pt-3">
+          Or continue with another account
+        </div>
+        <FacebookLoginButton className="mt-3 mb-3"/>
+        <GoogleLoginButton className="mt-3 mb-3"/>
+        <div className="text-center">
+          <a href="/sign-up">Sign up</a>
+          <span className="p-2">|</span>
+          <a href="/forgot-password">Forgot Password</a>
+        </div>
+      </Form>
+    );
+  }
 }
 
 export default App;
